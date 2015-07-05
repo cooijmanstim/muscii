01cbf67bcc9e6211253af828dea0e07c80d3d86e
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2015-07-05 17:11:25 -0400

    write some funky music

--- main.py
+++ main.py
@@ -1,64 +1,86 @@
-import re
-import collections
-import copy
-import fluidsynth
-import itertools as it
 import logging
-import time
-
 logging.basicConfig()
 logger = logging.getLogger(__name__)
+
+import copy
+import itertools as it
 
 import music as mu
 import matrix as mx
 import events as ev
+import playback as pb
 
-melodies = mx.parse_matrix("""=e5  .  . zz  .
-                              =c5 z. z.  .  .
-                              =a4  zz zz .  .
-                              =g4  .  .  . zz""")
-channels = dict(z=0)
+voice_sequences = dict()
 
-events = []
-for i in xrange(4):
-    # repeat every four three-beat bars
-    t = i * 3 * 4
-    for melody in melodies:
-        channel = channels[melody.name]
-        events.extend(melody.get_events(channel, t))
+voice_sequences.update(mx.parse_matrix("""
+&WoodBlock  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+&Claves     .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+&Maraca     .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+&Cabasa     .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+&PedalHihat .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+&SideStick  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+&BassDrum1  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+"""))
 
-def sortfun(event):
-    if isinstance(event, ev.NoteOff):
-        # ensure noteoffs occur before noteons so that new noteons
-        # are not immediately cancelled.
-        return event.time - 1e-5
-    else:
-        return event.time
-events.sort(key=sortfun)
+#   C6      D7      GM7     E-      CM7     F#      B-      B7      E       AM7     Eb-     G#      C#-     AM7     G#
+voice_sequences.update(mx.parse_matrix("""
+=a5 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   vv  .   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   vvvv.v v. vv.v  . vvvv  .   v   .   .   .   .   .   .   .   .   
+=g5 .   .   .   .   .   .   .   .   .   .   .  v.   .   .   .   .   .   . v .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   . v vv  .   .   .   .   .   .   .   . v vv  .   .   .v  .   .   .   .   .   .   .   .   
+=f5 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+=e5 .   .   .   .   .   .   .   .   vvvvvvvv.   . vvvvv .   .   .   .   .   .   .  v.   .   .   . v .   .  v.  v.   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .  v.  vvvv . v vvv .   .   .   .   .   
+=d5 .   vvvvvvvvvv  .   .   .   .  v.   .   .   .   .  vvvvv.   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .  vv   .   vvv .   .   .   .   
+=c5 .   .   .   . vvvvvv.   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . v .   .   .   .   .   vvvvvvvvvvvv.   
+=b4 .   .   .   .   .   vvvvvvvv. v .   .   .   .   .   .   .   . vv.   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .    
+    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+=a4 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .v  .   .   .   .   .   .   .   .   .
+=g4 .   .   .   .   .   .   .   .v  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .     
+"""))
 
-fs = fluidsynth.Synth()
-fs.start(driver="alsa")
+#   C6  D7  GM7 E-  CM7 F#  B-  B7  E   AM7 Eb- G#  C#- AM7 G#
+voice_sequences.update(mx.parse_matrix("""
+    .   .   .   .   .   .   .   .   .   .   .   wwwwwwwwwwwwwwwwwwww
+=g5 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .     
+    .   .   .   .   .   .   .   .   .   .   wwww.   .   .   .   .   
+=f5 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+=e5 .   .   .   .   . wwwwww.   .   wwwwwwww.   .   uuuuuuuu.   .   
+    .   .   .   .   .   .   .   wwww.   .   .   .   .   .   uuuuuuuu
+=d5 .   .   .   .   .   .   wwww.   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   xxxxxxxx.   xxxxxxxxxxxx.   
+=c5 .   xxxxxx  .   ww  .   .   .   .   .   .   xxxx.   .   .   xxxx
+=b4 .   .   . xxxxxxxxxx.   xxxxxxxxxxxx.   .   .   .   .   .   .   
+    .   .   .   .   .   xxxx.   .   .   .   .   .   .   .   .   .   
+=a4 xxxx.   .   .   .   .   .   .   .   yyyyyyyy.   .   .   .   .   
+    .   .   .   .   .   .   .   .   yyyy.   .   yyyyyyyyyyyyyyyyyyyy
+=g4 .   .   .   yyyyyyyy.   .   .   .   .   .   .   .   .   .   .   
+    .   yyyyyyyy.   .   yyyyyyyyyyyy.   .   .   .   .   .   .   .   
+=f4 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+=e4 yyyy.   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   zzz .   .   .   .   .   
+=d4 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   .   .   zzzz.   .   .   
+=c4 zzzz.   .   .   zzzz.   .   .   .   .   .   .   .   .   .   .   
+=b3 .   .   .   .   .   .   zzzz.   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+=a3 .   zzzz.   .   .   .   .   zzzz.   zzzz.  z.   .   zzzz.   .   
+    .   .   .   .   .   .   .   .   .   .   .   zzzz.   .   zzzzzzzz
+=g3 .   .   zzzz.   .   .   .   .   .   .   .   .   .   .   .   .   
+    .   .   .   .   .   zzzz.   .   .   .   .   .   .   .   .   .   
+=f3 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   
+=e3 .   .   .   zzzz.   .   .   .   zzzz.   .   .   .   .   .   .   
+""", transforms=[mu.Scale("duration", 2)]))
 
-kbhstrings = fs.sfload("/home/tim/SF/strings/kbh-strings.sf2")
-florestan_woodwinds = fs.sfload("/home/tim/SF/wind/Woodwinds/florestan_woodwinds.sf2")
-
-instruments = dict(
-    strings=(kbhstrings, 0, 0),
-    bassoon=(florestan_woodwinds, 0, 70),
+voice_instruments = dict(
+    z="pizzicato contrabass drawbar_organ",
+    y="cello tuba",
+    x="viola trombone",
+    w="violin french_horns",
+    u="slow_violin",
+    v="jazz_guitar",
 )
 
-fs.program_select(0, *instruments["strings"])
-fs.program_select(1, *instruments["strings"])
-
-bpm = 140
-beat_duration = 60.0/bpm
-t = events[0].time
-for event in events:
-    dt = event.time - t
-    if dt > 0:
-        time.sleep(dt * beat_duration)
-    t = event.time
-    logger.warn("event %s from %s" % (event, event.source))
-    event.occur(fs)
-
-fs.delete()
+pb.play_voices(voice_sequences, voice_instruments)
